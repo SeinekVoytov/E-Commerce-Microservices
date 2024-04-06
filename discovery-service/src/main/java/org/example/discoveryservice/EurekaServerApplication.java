b6df022037e8afbfead43cd140ae52a3046cbd37
SeinekVoytov <voytov1704@gmail.com>
2024-04-06 18:05:38 +0300

    rename module eureka-server to discovery-service to satisfy requirements

--- discovery-service/src/main/java/org/example/discoveryservice/EurekaServerApplication.java
+++ discovery-service/src/main/java/org/example/discoveryservice/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.example.eurekaserver;
+package org.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
