b1bd635a083198d08349cecb33151ef42491f777
SeinekVoytov <voytov1704@gmail.com>
2024-05-06 23:50:32 +0300

    FIX: invalid method signatures for findOrderLongByUserId method in repositories

--- order-service/src/main/java/org/example/orderservice/repository/OrderLongRepository.java
+++ order-service/src/main/java/org/example/orderservice/repository/OrderLongRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface OrderLongRepository extends JpaRepository<OrderLong, Integer> {
-    List<OrderLong> findOrderLongsByUserId();
+    List<OrderLong> findOrderLongsByUserId(UUID userId);
 }
--- order-service/src/main/java/org/example/orderservice/repository/OrderShortRepository.java
+++ order-service/src/main/java/org/example/orderservice/repository/OrderShortRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface OrderShortRepository extends JpaRepository<OrderShort, Integer> {
-    List<OrderShort> findOrderLongsByUserId();
+    List<OrderShort> findOrderLongsByUserId(UUID userId);
 }
