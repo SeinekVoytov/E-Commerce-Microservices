61eee768f0904946bd7809bee1cd1650d4d3194c
SeinekVoytov <voytov1704@gmail.com>
2024-05-08 23:40:12 +0300

    FIX: custom method name in OrderShortRepository

--- order-service/src/main/java/org/example/orderservice/repository/OrderShortRepository.java
+++ order-service/src/main/java/org/example/orderservice/repository/OrderShortRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface OrderShortRepository extends JpaRepository<OrderShort, Integer> {
-    List<OrderShort> findOrderLongsByUserId(UUID userId);
+    List<OrderShort> findOrderShortsByUserId(UUID userId);
 }
