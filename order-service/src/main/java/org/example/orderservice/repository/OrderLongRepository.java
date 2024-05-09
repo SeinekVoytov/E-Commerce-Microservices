506ca74a4e3b7af54a38bb5e81b9823f4f0e8064
SeinekVoytov <voytov1704@gmail.com>
2024-05-09 15:59:05 +0300

    FIX: custom method signature

--- order-service/src/main/java/org/example/orderservice/repository/OrderLongRepository.java
+++ order-service/src/main/java/org/example/orderservice/repository/OrderLongRepository.java
@@ -4,10 +4,10 @@ import org.example.orderservice.model.order.OrderLong;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface OrderLongRepository extends JpaRepository<OrderLong, Integer> {
-    List<OrderLong> findOrderLongsByUserId(UUID userId);
+    Optional<OrderLong> findOrderLongByIdAndUserId(int id, UUID userId);
 }
