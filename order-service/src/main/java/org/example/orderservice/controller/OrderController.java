34131fa695aaa21cf135e7c96c93bc78058bd381
SeinekVoytov <voytov1704@gmail.com>
2024-05-09 15:43:55 +0300

    provide controller layer

---
+++ order-service/src/main/java/org/example/orderservice/controller/OrderController.java
@@ -0,0 +1,30 @@
+package org.example.orderservice.controller;
+
+import org.example.orderservice.dto.order.OrderShortDto;
+import org.example.orderservice.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderShortDto>> getOrderShortsByUserId(Authentication authentication) {
+        List<OrderShortDto> foundOrders = orderService.getUserOrdersShort(authentication);
+        return ResponseEntity.ok(foundOrders);
+    }
+}
