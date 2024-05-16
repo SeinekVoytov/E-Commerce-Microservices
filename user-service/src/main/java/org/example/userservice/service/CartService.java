7e4986e6d65ef29eae1d2d26fe7f2bb8fcd046ba
SeinekVoytov <voytov1704@gmail.com>
2024-05-16 18:32:24 +0300

    add CartService interface with basic methods

--- user-service/src/main/java/org/example/userservice/service/CartService.java
+++ user-service/src/main/java/org/example/userservice/service/CartService.java
@@ -1,2 +1,13 @@
-package org.example.userservice.service;public interface CartService {
+package org.example.userservice.service;
+
+import org.example.userservice.dto.CartItemRequest;
+import org.example.userservice.dto.CartItemResponse;
+import org.springframework.security.core.Authentication;
+
+public interface CartService {
+
+    CartItemResponse addItemToCart(Authentication auth, CartItemRequest request);
+    void updateItemQuantity(Authentication auth, CartItemRequest request);
+    CartItemResponse deleteItemFromCart(Authentication auth, CartItemRequest request);
+    void order(Authentication auth);
 }
