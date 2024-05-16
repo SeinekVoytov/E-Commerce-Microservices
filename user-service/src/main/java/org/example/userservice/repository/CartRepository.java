cb2b8a05d5de900f2535fbf9c3bcd423b314ae6d
SeinekVoytov <voytov1704@gmail.com>
2024-05-16 16:43:56 +0300

    add repository for cart class

---
+++ user-service/src/main/java/org/example/userservice/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package org.example.userservice.repository;
+
+import org.example.userservice.model.cart.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+}
