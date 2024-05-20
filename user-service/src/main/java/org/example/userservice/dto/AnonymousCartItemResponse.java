64fcb75cb27c9f2ec138cfd6498bc6091fafc675
SeinekVoytov <voytov1704@gmail.com>
2024-05-20 16:01:17 +0300

    FIX: replace product field type with ProductLongDto

--- user-service/src/main/java/org/example/userservice/dto/AnonymousCartItemResponse.java
+++ user-service/src/main/java/org/example/userservice/dto/AnonymousCartItemResponse.java
@@ -1,12 +1,10 @@
 package org.example.userservice.dto;
 
-import org.example.userservice.model.product.ProductLong;
-
 import java.util.UUID;
 
 public record AnonymousCartItemResponse (
         UUID cartId,
-        ProductLong product,
+        ProductLongDto product,
         int quantity
 ) {
 }
