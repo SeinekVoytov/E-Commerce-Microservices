918118d8347f1211fac0e9b401f95310fb24d40d
SeinekVoytov <voytov1704@gmail.com>
2024-06-05 10:17:11 +0300

    refactor models. alter init.sql accordingly

--- docker/init.sql
+++ docker/init.sql
@@ -1,40 +1,47 @@
 \c e_commerce_microservices;
 
 CREATE TABLE IF NOT EXISTS price (
-    id SERIAL PRIMARY KEY,
-    amount REAL NOT NULL CHECK ( amount > 0 ),
+    id INT PRIMARY KEY,
+    amount NUMERIC NOT NULL CHECK ( amount > 0 ),
     currency CHARACTER(3) NOT NULL
 );
 
+CREATE SEQUENCE IF NOT EXISTS price_seq START 1 INCREMENT 20 OWNED BY price.id;
+
 CREATE TABLE IF NOT EXISTS category (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY,
 --     parent_category_id INT DEFAULT NULL,
     name TEXT NOT NULL UNIQUE,
     count INT NOT NULL CHECK ( count >= 0 ) DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS product_short (
-    id SERIAL PRIMARY KEY,
+CREATE SEQUENCE IF NOT EXISTS category_seq START 1 INCREMENT 20 OWNED BY category.id;
+
+CREATE TABLE IF NOT EXISTS product (
+    id INT PRIMARY KEY,
     name TEXT NOT NULL,
-    img_uri TEXT NOT NULL,
     price_id INT REFERENCES price (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS product_short_category (
-    product_short_id INT REFERENCES product_short (id),
+CREATE SEQUENCE IF NOT EXISTS product_seq START 1 INCREMENT 20 OWNED BY product.id;
+
+CREATE TABLE IF NOT EXISTS product_category (
+    product_id INT REFERENCES product (id),
     category_id INT REFERENCES category (id),
-    PRIMARY KEY (product_short_id, category_id)
+    PRIMARY KEY (product_id, category_id)
 );
 
-CREATE TABLE IF NOT EXISTS product_long (
-    id SERIAL PRIMARY KEY,
-    product_short_id INT REFERENCES product_short (id) ON DELETE CASCADE,
-    length_m REAL NOT NULL CHECK ( length_m > 0 ),
-    width_m REAL NOT NULL CHECK ( width_m > 0 ),
-    height_m REAL NOT NULL CHECK ( height_m > 0 ),
-    net_weight_kg REAL NOT NULL CHECK ( net_weight_kg > 0 ),
-    gross_weight_kg REAL NOT NULL CHECK ( gross_weight_kg > 0 )
+CREATE TABLE IF NOT EXISTS product_details (
+    id INT PRIMARY KEY,
+    product_id INT REFERENCES product (id) ON DELETE CASCADE,
+    length_meters DOUBLE PRECISION NOT NULL CHECK ( length_meters > 0 ),
+    width_meters DOUBLE PRECISION NOT NULL CHECK ( width_meters > 0 ),
+    height_meters DOUBLE PRECISION NOT NULL CHECK ( height_meters > 0 ),
+    net_weight_kg DOUBLE PRECISION NOT NULL CHECK ( net_weight_kg > 0 ),
+    gross_weight_kg DOUBLE PRECISION NOT NULL CHECK ( gross_weight_kg > 0 )
 );
+
+CREATE SEQUENCE IF NOT EXISTS product_details_seq START 1 INCREMENT 20 OWNED BY product_details.id;
 
 CREATE OR REPLACE FUNCTION increment_category_count() RETURNS TRIGGER AS $$
 BEGIN
@@ -50,15 +57,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER tr_insert AFTER INSERT ON product_short_category FOR EACH ROW EXECUTE PROCEDURE increment_category_count();
+CREATE OR REPLACE TRIGGER tr_insert AFTER INSERT ON product_category FOR EACH ROW EXECUTE PROCEDURE increment_category_count();
 
-CREATE OR REPLACE TRIGGER tr_delete AFTER DELETE ON product_short_category FOR EACH ROW EXECUTE PROCEDURE decrement_category_count();
+CREATE OR REPLACE TRIGGER tr_delete AFTER DELETE ON product_category FOR EACH ROW EXECUTE PROCEDURE decrement_category_count();
 
 CREATE TABLE IF NOT EXISTS image (
-    id SERIAL PRIMARY KEY,
-    owner_id INT REFERENCES product_short (id),
-    url TEXT NOT NULL
+    id INT PRIMARY KEY,
+    owner_id INT REFERENCES product (id),
+    url VARCHAR(2048) NOT NULL
 );
+
+CREATE SEQUENCE IF NOT EXISTS image_seq START 1 INCREMENT 20 OWNED BY image.id;
 
 CREATE TABLE IF NOT EXISTS fee (
     id SERIAL PRIMARY KEY,
@@ -69,47 +78,55 @@ CREATE TABLE IF NOT EXISTS fee (
 CREATE TYPE delivery_type AS ENUM ('STANDARD', 'EXPRESS', 'SAME_DAY', 'NEXT_DAY', 'SCHEDULED', 'IN_STORE_PICKING');
 CREATE TYPE delivery_status AS ENUM ('ORDER_RECEIVED', 'ORDER_PROCESSING', 'IN_TRANSIT', 'DELIVERED', 'CANCELLED');
 
+CREATE TABLE IF NOT EXISTS fee (
+    id INT PRIMARY KEY,
+    amount REAL NOT NULL CHECK ( amount > 0 ),
+    currency CHARACTER(3) NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS fee_seq START 1 INCREMENT 20 OWNED BY fee.id;
+
 CREATE TABLE IF NOT EXISTS delivery (
-    id SERIAL PRIMARY KEY,
-    fee_id INT REFERENCES fee (id) ON DELETE CASCADE,
+    id INT PRIMARY KEY,
+    fee_id INT REFERENCES fee (id),
     status delivery_status NOT NULL,
     type delivery_type NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS quantity (
-    id SERIAL PRIMARY KEY,
+CREATE SEQUENCE IF NOT EXISTS delivery_seq START 1 INCREMENT 20 OWNED BY delivery.id;
+
+CREATE TABLE IF NOT EXISTS "order" (
+    id INT PRIMARY KEY,
+    delivery_id INT REFERENCES delivery (id),
+    user_id UUID NOT NULL
+);
+
+CREATE SEQUENCE IF NOT EXISTS order_seq START 1 INCREMENT 20 OWNED BY "order".id;
+
+CREATE TABLE IF NOT EXISTS order_item (
+    id INT PRIMARY KEY,
+    order_id INT REFERENCES "order" (id),
+    item_id INT REFERENCES product_details (id),
     quantity INT NOT NULL CHECK ( quantity > 0 )
 );
 
-CREATE TABLE IF NOT EXISTS order_short (
-    id SERIAL PRIMARY KEY,
-    delivery_id INT REFERENCES delivery (id) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS order_item_short (
-    id SERIAL PRIMARY KEY,
-    order_id INT REFERENCES order_short (id),
-    item_id INT REFERENCES product_short (id),
-    quantity_id INT REFERENCES quantity (id)
-);
+CREATE SEQUENCE IF NOT EXISTS order_item_seq START 1 INCREMENT 50 OWNED BY order_item.id;
 
 CREATE TABLE IF NOT EXISTS address (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY,
     city VARCHAR(64) NOT NULL,
     country VARCHAR(64) NOT NULL,
     street_address TEXT NOT NULL,
     apartment TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS order_long (
-    id SERIAL PRIMARY KEY,
-    address_id INT REFERENCES address (id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL
+CREATE SEQUENCE IF NOT EXISTS address_seq START 1 INCREMENT 20 OWNED BY address.id;
+
+CREATE TABLE IF NOT EXISTS order_details (
+    id INT PRIMARY KEY,
+    order_id INT REFERENCES "order" (id),
+    address_id INT REFERENCES address (id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS order_item_long (
-    id SERIAL PRIMARY KEY REFERENCES order_item_short (id) ON DELETE CASCADE,
-    order_id INT REFERENCES order_long (id),
-    item_id INT REFERENCES product_long (id),
-    quantity_id INT REFERENCES quantity (id)
-);
+CREATE SEQUENCE IF NOT EXISTS order_details_seq START 1 INCREMENT 20 OWNED BY order_details.id;
---
+++ product-service/src/main/java/org/example/productservice/model/Product.java
@@ -0,0 +1,60 @@
+package org.example.productservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "owner_id", referencedColumnName = "id")
+    private Set<Image> images;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "price_id", referencedColumnName = "id")
+    private Price price;
+
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(name = "product_short_category",
+            joinColumns = @JoinColumn(name = "product_short_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
+
+    @PreRemove
+    private void removeMappingWithCategories() {
+        categories.forEach(
+                category -> category.getProducts().remove(this)
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(images, product.images) && Objects.equals(price, product.price) && Objects.equals(categories, product.categories);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, images, price, categories);
+    }
+}
