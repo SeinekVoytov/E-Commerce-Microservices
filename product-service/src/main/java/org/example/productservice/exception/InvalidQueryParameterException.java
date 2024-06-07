7445bc92182cf864d43d638940a5ac05af45ee74
SeinekVoytov <voytov1704@gmail.com>
2024-06-07 17:09:20 +0300

    add message to InvalidQueryParameterException

--- product-service/src/main/java/org/example/productservice/exception/InvalidQueryParameterException.java
+++ product-service/src/main/java/org/example/productservice/exception/InvalidQueryParameterException.java
@@ -2,9 +2,9 @@ package org.example.productservice.exception;
 
 public class InvalidQueryParameterException extends RuntimeException {
 
-    private static final String MESSAGE = "Invalid query parameter value";
+    private static final String MESSAGE = "Invalid value '%s' for parameter '%s'";
 
-    public InvalidQueryParameterException() {
-        super(MESSAGE);
+    public InvalidQueryParameterException(String paramName, String paramValue) {
+        super(String.format(MESSAGE, paramValue, paramName));
     }
 }
