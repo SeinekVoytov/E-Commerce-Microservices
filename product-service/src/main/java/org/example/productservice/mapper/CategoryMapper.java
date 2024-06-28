f77f84e649848727e58d9ad5adb77b68a234988e
SeinekVoytov <voytov1704@gmail.com>
2024-06-28 11:49:40 +0300

    Mapper improvements:
    1) provide mapping for new dto's
    2) FIX: invalid non-recursive mapping for CategoryWithChildrenDto

--- product-service/src/main/java/org/example/productservice/mapper/CategoryMapper.java
+++ product-service/src/main/java/org/example/productservice/mapper/CategoryMapper.java
@@ -2,11 +2,17 @@ package org.example.productservice.mapper;
 
 import org.example.productservice.dto.CategoryDto;
 import org.example.productservice.dto.CategoryWithChildrenDto;
+import org.example.productservice.dto.CategoryWithParentDto;
 import org.example.productservice.model.Category;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = "spring",
@@ -20,7 +26,37 @@ public interface CategoryMapper {
 
     Set<Category> toEntitiesSet(Set<CategoryDto> dtos);
 
+    @Mapping(source = "childCategories", target = "children", qualifiedByName = "mapChildren")
     CategoryWithChildrenDto toDtoWithChildren(Category entity);
 
     Set<CategoryWithChildrenDto> toDtoWithChildrenSet(Set<Category> entities);
+
+    @Named("mapChildren")
+    default Set<CategoryWithChildrenDto> mapChildren(Set<Category> entities) {
+
+        if (entities == null) {
+            return null;
+        }
+
+        return entities.stream()
+                .map(this::toDtoWithChildren)
+                .collect(Collectors.toSet());
+    }
+
+    @Mapping(source = "parentCategory", target = "parent", qualifiedByName = "mapParent")
+    CategoryWithParentDto toDtoWithParent(Category entity);
+
+    @Named("mapParent")
+    default CategoryWithParentDto mapParent(Category entity) {
+
+        if (entity == null) {
+            return null;
+        }
+
+        return new CategoryWithParentDto(
+                entity.getId(),
+                entity.getName(),
+                mapParent(entity.getParentCategory())
+        );
+    }
 }
