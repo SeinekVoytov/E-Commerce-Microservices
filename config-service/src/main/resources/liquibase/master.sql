01fff2218e64eb1740a6535d0d9e9bad3c0510ee
SeinekVoytov <voytov1704@gmail.com>
2024-07-04 14:26:44 +0300

    switch from init.sql file for database to migrations using liquibase

--- config-service/src/main/resources/liquibase/master.sql
+++ config-service/src/main/resources/liquibase/master.sql
@@ -1,5 +1,6 @@
-\c e_commerce_microservices;
+--liquibase formatted sql
 
+--changeset SeinekVoytov:1 dbms:postgresql
 CREATE TABLE IF NOT EXISTS price (
     id INT PRIMARY KEY,
     amount NUMERIC NOT NULL CHECK ( amount > 0 ),
@@ -7,7 +8,10 @@ CREATE TABLE IF NOT EXISTS price (
 );
 
 CREATE SEQUENCE IF NOT EXISTS price_seq START 1 INCREMENT 20 OWNED BY price.id;
+--rollback DROP SEQUENCE IF EXISTS price_seq;
+--rollback DROP TABLE IF EXISTS price;
 
+--changeset SeinekVoytov:2 dbms:postgresql
 CREATE TABLE IF NOT EXISTS category (
     id INT PRIMARY KEY,
     parent_category_id INT DEFAULT NULL REFERENCES category (id),
@@ -15,7 +19,10 @@ CREATE TABLE IF NOT EXISTS category (
 );
 
 CREATE SEQUENCE IF NOT EXISTS category_seq START 1 INCREMENT 20 OWNED BY category.id;
+--rollback DROP SEQUENCE IF EXISTS category_seq;
+--rollback DROP TABLE IF EXISTS category;
 
+--changeset SeinekVoytov:3 dbms:postgresql
 CREATE TABLE IF NOT EXISTS product (
     id INT PRIMARY KEY,
     name TEXT NOT NULL,
@@ -23,13 +30,18 @@ CREATE TABLE IF NOT EXISTS product (
 );
 
 CREATE SEQUENCE IF NOT EXISTS product_seq START 1 INCREMENT 20 OWNED BY product.id;
+--rollback DROP SEQUENCE IF EXISTS product_seq;
+--rollback DROP TABLE IF EXISTS product;
 
+--changeset SeinekVoytov:4 dbms:postgresql
 CREATE TABLE IF NOT EXISTS product_category (
     product_id INT REFERENCES product (id),
     category_id INT REFERENCES category (id),
     PRIMARY KEY (product_id, category_id)
 );
+--rollback DROP TABLE IF EXISTS product_category;
 
+--changeset SeinekVoytov:5 dbms:postgresql
 CREATE TABLE IF NOT EXISTS product_details (
     id INT PRIMARY KEY,
     product_id INT REFERENCES product (id) ON DELETE CASCADE,
@@ -41,7 +53,10 @@ CREATE TABLE IF NOT EXISTS product_details (
 );
 
 CREATE SEQUENCE IF NOT EXISTS product_details_seq START 1 INCREMENT 20 OWNED BY product_details.id;
+--rollback DROP SEQUENCE IF EXISTS product_details_seq;
+--rollback DROP TABLE IF EXISTS product_details;
 
+--changeset SeinekVoytov:6 dbms:postgresql
 CREATE TABLE IF NOT EXISTS image (
     id INT PRIMARY KEY,
     owner_id INT REFERENCES product (id),
@@ -49,7 +64,10 @@ CREATE TABLE IF NOT EXISTS image (
 );
 
 CREATE SEQUENCE IF NOT EXISTS image_seq START 1 INCREMENT 20 OWNED BY image.id;
+--rollback DROP SEQUENCE IF EXISTS image_seq;
+--rollback DROP TABLE IF EXISTS image;
 
+--changeset SeinekVoytov:7 dbms:postgresql
 CREATE TABLE IF NOT EXISTS fee (
     id INT PRIMARY KEY,
     amount DECIMAL NOT NULL CHECK ( amount > 0 ),
@@ -57,10 +75,27 @@ CREATE TABLE IF NOT EXISTS fee (
 );
 
 CREATE SEQUENCE IF NOT EXISTS fee_seq START 1 INCREMENT 20 OWNED BY fee.id;
+--rollback DROP SEQUENCE IF EXISTS fee_seq;
+--rollback DROP TABLE IF EXISTS fee;
 
-CREATE TYPE delivery_type AS ENUM ('STANDARD', 'EXPRESS', 'SAME_DAY', 'NEXT_DAY', 'SCHEDULED', 'IN_STORE_PICKING');
-CREATE TYPE delivery_status AS ENUM ('ORDER_RECEIVED', 'ORDER_PROCESSING', 'IN_TRANSIT', 'DELIVERED', 'CANCELLED');
+--changeset SeinekVoytov:8 dbms:postgresql endDelimiter:$$
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'delivery_type') THEN
+            CREATE TYPE delivery_type AS ENUM ('STANDARD', 'EXPRESS', 'SAME_DAY', 'NEXT_DAY', 'SCHEDULED', 'IN_STORE_PICKING');
+        END IF;
+    END $$;
 
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'delivery_status') THEN
+            CREATE TYPE delivery_status AS ENUM ('ORDER_RECEIVED', 'ORDER_PROCESSING', 'IN_TRANSIT', 'DELIVERED', 'CANCELLED');
+        END IF;
+    END $$;
+--rollback DROP TYPE IF EXISTS delivery_status;
+--rollback DROP TYPE IF EXISTS delivery_type;
+
+--changeset SeinekVoytov:9 dbms:postgresql
 CREATE TABLE IF NOT EXISTS delivery (
     id INT PRIMARY KEY,
     fee_id INT REFERENCES fee (id),
@@ -70,6 +105,12 @@ CREATE TABLE IF NOT EXISTS delivery (
 
 CREATE SEQUENCE IF NOT EXISTS delivery_seq START 1 INCREMENT 20 OWNED BY delivery.id;
 
+CREATE SEQUENCE IF NOT EXISTS delivery_seq START 1 INCREMENT 20 OWNED BY delivery.id;
+--rollback DROP SEQUENCE IF EXISTS delivery_seq;
+--rollback DROP TABLE IF EXISTS delivery;
+
+
+--changeset SeinekVoytov:10 dbms:postgresql
 CREATE TABLE IF NOT EXISTS "order" (
     id INT PRIMARY KEY,
     delivery_id INT REFERENCES delivery (id),
@@ -78,7 +119,22 @@ CREATE TABLE IF NOT EXISTS "order" (
 );
 
 CREATE SEQUENCE IF NOT EXISTS order_seq START 1 INCREMENT 20 OWNED BY "order".id;
+--rollback DROP SEQUENCE IF EXISTS order_seq;
+--rollback DROP TABLE IF EXISTS "order";
 
+--changeset SeinekVoytov:11 dbms:postgresql
+CREATE TABLE IF NOT EXISTS "order" (
+    id INT PRIMARY KEY,
+    delivery_id INT REFERENCES delivery (id),
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE SEQUENCE IF NOT EXISTS order_seq START 1 INCREMENT 20 OWNED BY "order".id;
+--rollback DROP SEQUENCE IF EXISTS order_seq;
+--rollback DROP TABLE IF EXISTS "order";
+
+--changeset SeinekVoytov:12 dbms:postgresql
 CREATE TABLE IF NOT EXISTS order_item (
     id INT PRIMARY KEY,
     order_id INT REFERENCES "order" (id),
@@ -87,7 +143,10 @@ CREATE TABLE IF NOT EXISTS order_item (
 );
 
 CREATE SEQUENCE IF NOT EXISTS order_item_seq START 1 INCREMENT 50 OWNED BY order_item.id;
+--rollback DROP SEQUENCE IF EXISTS order_item_seq;
+--rollback DROP TABLE IF EXISTS order_item;
 
+--changeset SeinekVoytov:13 dbms:postgresql
 CREATE TABLE IF NOT EXISTS address (
     id INT PRIMARY KEY,
     city VARCHAR(64) NOT NULL,
@@ -97,7 +156,10 @@ CREATE TABLE IF NOT EXISTS address (
 );
 
 CREATE SEQUENCE IF NOT EXISTS address_seq START 1 INCREMENT 20 OWNED BY address.id;
+--rollback DROP SEQUENCE IF EXISTS address_seq;
+--rollback DROP TABLE IF EXISTS address;
 
+--changeset SeinekVoytov:14 dbms:postgresql
 CREATE TABLE IF NOT EXISTS order_details (
     id INT PRIMARY KEY,
     order_id INT REFERENCES "order" (id),
@@ -106,14 +168,19 @@ CREATE TABLE IF NOT EXISTS order_details (
 );
 
 CREATE SEQUENCE IF NOT EXISTS order_details_seq START 1 INCREMENT 20 OWNED BY order_details.id;
+--rollback DROP SEQUENCE IF EXISTS order_details;
+--rollback DROP TABLE IF EXISTS order_details_seq;
 
+--changeset SeinekVoytov:15 dbms:postgresql
 CREATE TABLE IF NOT EXISTS cart (
     id UUID PRIMARY KEY,
     user_id UUID UNIQUE,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+--rollback DROP TABLE IF EXISTS cart;
 
+--changeset SeinekVoytov:16 dbms:postgresql
 CREATE TABLE IF NOT EXISTS cart_item (
     id INT PRIMARY KEY,
     product_id INT REFERENCES product (id),
@@ -121,8 +188,11 @@ CREATE TABLE IF NOT EXISTS cart_item (
     cart_id UUID REFERENCES cart (id)
 );
 
-CREATE SEQUENCE cart_item_seq START 1 INCREMENT 75 OWNED BY cart_item.id;
+CREATE SEQUENCE IF NOT EXISTS cart_item_seq START 1 INCREMENT 75 OWNED BY cart_item.id;
+--rollback DROP SEQUENCE cart_item_seq;
+--rollback DROP TABLE cart_item;
 
+--changeset SeinekVoytov:17 dbms:postgresql splitStatements:false
 CREATE OR REPLACE FUNCTION on_item_added_to_cart() RETURNS TRIGGER AS $$
 BEGIN
     UPDATE cart SET updated_at = CURRENT_TIMESTAMP WHERE NEW.cart_id = id;
@@ -132,3 +202,5 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER tr_update_updated_at AFTER INSERT OR DELETE ON cart_item FOR EACH ROW EXECUTE PROCEDURE on_item_added_to_cart();
+--rollback DROP TRIGGER IF EXISTS tr_update_updated_at ON cart_item;
+--rollback DROP FUNCTION IF EXISTS on_item_added_to_cart();
