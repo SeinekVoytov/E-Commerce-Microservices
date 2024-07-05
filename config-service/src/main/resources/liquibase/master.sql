2233e78759d60f609ece57085ad1e2c44275ea9f
SeinekVoytov <voytov1704@gmail.com>
2024-07-05 19:27:07 +0300

    FIX: old product table structure

--- config-service/src/main/resources/liquibase/master.sql
+++ config-service/src/main/resources/liquibase/master.sql
@@ -26,6 +26,8 @@ CREATE SEQUENCE IF NOT EXISTS category_seq START 1 INCREMENT 20 OWNED BY categor
 CREATE TABLE IF NOT EXISTS product (
     id INT PRIMARY KEY,
     name TEXT NOT NULL,
+    net_weight_kg DOUBLE PRECISION NOT NULL CHECK ( net_weight_kg > 0 ),
+    description TEXT NOT NULL,
     price_id INT REFERENCES price (id) ON DELETE CASCADE
 );
 
@@ -48,7 +50,6 @@ CREATE TABLE IF NOT EXISTS product_details (
     length_meters DOUBLE PRECISION NOT NULL CHECK ( length_meters > 0 ),
     width_meters DOUBLE PRECISION NOT NULL CHECK ( width_meters > 0 ),
     height_meters DOUBLE PRECISION NOT NULL CHECK ( height_meters > 0 ),
-    net_weight_kg DOUBLE PRECISION NOT NULL CHECK ( net_weight_kg > 0 ),
     gross_weight_kg DOUBLE PRECISION NOT NULL CHECK ( gross_weight_kg > 0 )
 );
 
