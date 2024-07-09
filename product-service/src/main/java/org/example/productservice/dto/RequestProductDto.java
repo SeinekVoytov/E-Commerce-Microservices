dde40bd0a605745031c8f126db460d5492e1c068
SeinekVoytov <voytov1704@gmail.com>
2024-07-09 10:25:26 +0300

    avoid using wildcard in importing from jakarta.validation.constraints

--- product-service/src/main/java/org/example/productservice/dto/RequestProductDto.java
+++ product-service/src/main/java/org/example/productservice/dto/RequestProductDto.java
@@ -1,6 +1,9 @@
 package org.example.productservice.dto;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import org.hibernate.validator.constraints.URL;
 
