767d26d62e0fefe54741389df1c615c0e91db234
SeinekVoytov <voytov1704@gmail.com>
2024-07-18 12:44:31 +0300

    provide different constructors with message for ProductNotFoundException

--- product-service/src/main/java/org/example/productservice/exception/ProductNotFoundException.java
+++ product-service/src/main/java/org/example/productservice/exception/ProductNotFoundException.java
@@ -1,10 +1,17 @@
 package org.example.productservice.exception;
 
+import java.util.Collection;
+
 public class ProductNotFoundException extends RuntimeException {
 
-    private static final String MESSAGE = "Product could not be found";
+    private static final String MESSAGE_FORMAT_SINGLE_ID = "Product with id = %s could not be found";
+    private static final String MESSAGE_FORMAT_MULTIPLE_ID = "Product with ids = %s could not be found";
 
-    public ProductNotFoundException() {
-        super(MESSAGE);
+    public ProductNotFoundException(Integer id) {
+        super(String.format(MESSAGE_FORMAT_SINGLE_ID, id));
+    }
+
+    public ProductNotFoundException(Collection<Integer> ids) {
+        super(String.format(MESSAGE_FORMAT_MULTIPLE_ID, ids));
     }
 }
