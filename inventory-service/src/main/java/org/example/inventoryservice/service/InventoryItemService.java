ee2717364f51aad6eface9e628342e3588606fca
SeinekVoytov <voytov1704@gmail.com>
2024-07-22 13:07:38 +0300

    add item identifier to update method and make this method transactional

--- inventory-service/src/main/java/org/example/inventoryservice/service/InventoryItemService.java
+++ inventory-service/src/main/java/org/example/inventoryservice/service/InventoryItemService.java
@@ -4,6 +4,7 @@ import org.example.inventoryservice.dto.InventoryItemRequest;
 import org.example.inventoryservice.dto.InventoryItemResponse;
 import org.example.inventoryservice.dto.InventoryResponse;
 import org.example.inventoryservice.dto.UpdateInventoryItemQuantityRequest;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface InventoryItemService {
 
@@ -13,7 +14,9 @@ public interface InventoryItemService {
 
     InventoryItemResponse addInventoryItem(InventoryItemRequest request);
 
-    InventoryItemResponse updateInventoryItemQuantity(UpdateInventoryItemQuantityRequest request);
+    @Transactional
+    InventoryItemResponse updateInventoryItemQuantity(Integer productId,
+                                                      UpdateInventoryItemQuantityRequest request);
 
     InventoryItemResponse deleteInventoryItem(Integer productId);
 }
