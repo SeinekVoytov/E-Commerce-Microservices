33680af3dbad8227e3bc5704b655e0514f41d163
SeinekVoytov <voytov1704@gmail.com>
2024-07-22 13:31:56 +0300

    add operation type field to UpdateInventoryItemQuantityRequest

--- inventory-service/src/main/java/org/example/inventoryservice/dto/UpdateInventoryItemQuantityRequest.java
+++ inventory-service/src/main/java/org/example/inventoryservice/dto/UpdateInventoryItemQuantityRequest.java
@@ -1,6 +1,22 @@
 package org.example.inventoryservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public record UpdateInventoryItemQuantityRequest(
-        Integer quantity
+        Integer quantity,
+        OperationType operationType
 ) {
+
+    public enum OperationType {
+
+        INCREMENT,
+        DECREMENT,
+        SET;
+
+        @Override
+        @JsonValue
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
 }
